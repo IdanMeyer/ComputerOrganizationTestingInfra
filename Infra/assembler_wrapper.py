41a5bb1f9fbffa3ea1b44eb91744b9226b739f6b
Idan Meyer <idan@Idan-Laptop.localdomain>
2022-11-18 11:18:39 +0200

    Updated code a bit. not very stable

--- Infra/assembler_wrapper.py
+++ Infra/assembler_wrapper.py
@@ -23,11 +23,49 @@ class AssemblerTestRunner(object):
         self.assembler = Assembler(assembler_path, should_compile=should_compile)
 
 
+class Field(object):
+    def __init__(self, value, size):
+        self.value = value
+        self.size = size
+
+    def serialize(self):
+        return format(self.value, f'0{self.size}b')
+
+class FieldOp(Field):
+    def __init__(self, value):
+         super(FieldOp, self).__init__(value, 8)
+
+class FieldRd(Field):
+    def __init__(self, value):
+         super(FieldRd, self).__init__(value, 4)
+
+class FieldRs(Field):
+    def __init__(self, value):
+         super(FieldRs, self).__init__(value, 4)
+
+class FieldRt(Field):
+    def __init__(self, value):
+         super(FieldRt, self).__init__(value, 4)
+
 class CommandGeneral(ctypes.BigEndianStructure):
     _fields_ = [
         ("op", ctypes.c_uint32, 6),
         ("other", ctypes.c_uint32, 14),
     ]
+class CommandRFormat(object):
+    def __init__(self, op, rd, rs, rt):
+        self.op = FieldOp(op)
+        self.rd = FieldRd(op)
+        self.rs = FieldRs(op)
+        self.rt = FieldRt(op)
+
+    def serialize(self):
+        return "".join([
+            self.op.serialize(),
+            self.rd.serialize(),
+            self.rs.serialize(),
+            self.rt.serialize(),
+        ])
 
 class CommandIFormat(ctypes.BigEndianStructure):
     _fields_ = [
@@ -37,20 +75,15 @@ class CommandIFormat(ctypes.BigEndianStructure):
         ("rt", ctypes.c_uint32, 4),
         ("imm", ctypes.c_uint32, 20),
     ]
-    # _fields_ = [
-    #     ("op", ctypes.c_uint32, 6),
-    #     ("rs", ctypes.c_uint32, 5),
-    #     ("rt", ctypes.c_uint32, 5),
-    #     ("imm", ctypes.c_uint32, 16),
-    # ]
 
-class CommandRFormat(ctypes.BigEndianStructure):
-    _fields_ = [
-        ("op", ctypes.c_uint32, 8),
-        ("rd", ctypes.c_uint32, 4),
-        ("rs", ctypes.c_uint32, 4),
-        ("rt", ctypes.c_uint32, 4),
-    ]
+
+# class CommandRFormat(ctypes.BigEndianStructure):
+#     _fields_ = [
+#         ("op", ctypes.c_uint32, 8),
+#         ("rd", ctypes.c_uint32, 4),
+#         ("rs", ctypes.c_uint32, 4),
+#         ("rt", ctypes.c_uint32, 4),
+#     ]
     # _fields_ = [
     #     ("op", ctypes.c_uint32, 6),
     #     ("rs", ctypes.c_uint32, 5),
@@ -65,7 +98,7 @@ class CommandRFormat(ctypes.BigEndianStructure):
 #         ("op", ctypes.c_uint32, 6),
 #         ("address", ctypes.c_uint32, 26),
 #     ]
-COMMAND_TO_OPCODE = {
+OPCODE_TO_NUMBER = {
     "add" : 0,
     "sub" : 1,
     "mul" : 2,
@@ -137,13 +170,25 @@ class AssemblyLine(object):
         self.opcode, self.rd, self.rs, self.rt, self.imm = tuple(parts)
 
         aaa = self.pack()
+        print(aaa)
         # import ipdb;ipdb.set_trace()
 
     def pack(self):
-        packed_command = CommandJFormat()
-        packed_command.op = 1
-        packed_command.address = 2
-        return bin(struct.unpack_from('!I', packed_command)[0])
+        packed_command = CommandRFormat(OPCODE_TO_NUMBER[self.opcode],
+                                        REGISTER_TO_NUMBER[self.rd.strip(r"$")],
+                                        REGISTER_TO_NUMBER[self.rs.strip(r"$")],
+                                        REGISTER_TO_NUMBER[self.rt.strip(r"$")],
+                                        )
+        # packed_command.rd = REGISTER_TO_NUMBER[self.rd.strip(r"$")]
+        # packed_command.rs = REGISTER_TO_NUMBER[self.rs.strip(r"$")]
+        # packed_command.rt = REGISTER_TO_NUMBER[self.rt.strip(r"$")]
+        # packed_value = struct.unpack_from('!I', packed_command)[0]
+        import ipdb;ipdb.set_trace()
+        # return format(packed_value, '08b')
+        # return format(packed_value, '020b')
+
+        # Return data without 0b prefix
+        # return bin_data[2:]
 
 
     def assemble_line(self, raw_line):
--- tests/test_assembler.py
+++ tests/test_assembler.py
@@ -4,5 +4,5 @@ from Infra.assembler_wrapper import AssemblerTestRunner, AssemblyLine
 ASSEMBLER_PATH = "<assembler_path>"
 def test_assembler_sanity():
     runner = AssemblerTestRunner(ASSEMBLER_PATH)
-    a = AssemblyLine("L1: add $t3, $t2, $t1, 0")
+    a = AssemblyLine("L1: sub $t0, $t2, $t1, 0")
     print(a)
